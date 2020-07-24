7994bb8fd1cda5e7711bf4e09b30a64ee405dd2f
gtbu <30436307+g7sim@users.noreply.github.com>
2020-07-24 21:34:55 +0200

    smsimple template

---
+++ templates/sm-simpl-blue/js/index.js
@@ -0,0 +1,43 @@
+ $('#navigation li').click(function() {
+    $('#navigation li').addClass('selected');
+});
+
+$('.nav li > span').each(function() {
+var $this = $(this);
+$this.replaceWith('<a href="#" class="xhspan" onclick="return false" >' + $this.text() + '</a>');
+});
+
+$(function() {
+	$('#main-menu').smartmenus({
+		mainMenuSubOffsetX: -1,
+		subMenusSubOffsetX: 10,
+		subMenusSubOffsetY: 0
+	});
+});
+
+
+// SmartMenus mobile menu toggle button
+$(function() {
+  var $mainMenuState = $('#main-menu-state');
+  if ($mainMenuState.length) {
+    // animate mobile menu
+    $mainMenuState.change(function(e) {
+      var $menu = $('#main-menu');
+      if (this.checked) {
+        $menu.hide().slideDown(250, function() { $menu.css('display', 'block'); });
+      } else {
+        $menu.show().slideUp(250, function() { $menu.css('display', 'none'); });
+      }
+    });
+  // hide mobile menu beforeunload
+    $(window).bind('beforeunload unload', function() {
+      if ($mainMenuState[0].checked) {
+        $mainMenuState[0].click(); 
+      }
+    });
+	
+	 }
+});
+
+/* $( "input[name*='man']" ).val( "has man in it!" ); */
+$('[href^="#"]').addClass('cross').attr("onclick","return false");
