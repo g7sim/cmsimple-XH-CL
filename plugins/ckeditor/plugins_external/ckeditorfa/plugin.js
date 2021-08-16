8f572ff5b90a55093943a271c5e9b98324d5a10a
g7sim <manbu@mail.de>
2021-08-16 14:08:11 +0200

    ckeditor fa del

--- plugins/ckeditor/plugins_external/ckeditorfa/plugin.js
+++
@@ -1,9 +0,0 @@
-CKEDITOR.plugins.add('ckeditorfa5',{
-icons:'ckeditorfa',
-init:function(editor){
-editor.addCommand('ckeditorfa', new CKEDITOR.dialogCommand('ckeditorFaDialog',{allowedContent:'span(!fa-*)'}));
-editor.ui.addButton('ckeditorfa',{label:'FontAwesome5 icons',command:'ckeditorfa',toolbar:'insert',icon:this.path + 'icons/ckeditorfa.png'});
-CKEDITOR.dialog.add('ckeditorFaDialog', this.path + 'dialogs/ckeditorfa.js');
-CKEDITOR.document.appendStyleSheet(this.path + 'css/ckeditorfa.css');
-}
-});
