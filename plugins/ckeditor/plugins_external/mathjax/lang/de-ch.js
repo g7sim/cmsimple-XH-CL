abc855a8f60ab4b452ecee949489b7d60387459e
g7sim <manbu@mail.de>
2021-08-27 15:21:56 +0200

    Mathjax and dependencies added to ckeditor

---
+++ plugins/ckeditor/plugins_external/mathjax/lang/de-ch.js
@@ -0,0 +1,13 @@
+/*
+Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+*/
+CKEDITOR.plugins.setLang( 'mathjax', 'de-ch', {
+	title: 'Mathematik in TeX',
+	button: 'Rechnung',
+	dialogInput: 'Schreiben Sie hier in TeX',
+	docUrl: 'https://ckeditor.com/docs/ckeditor4/latest/features/mathjax.html',
+	docLabel: 'Mathjax-Dokumentation',
+	loading: 'Ladevorgang...',
+	pathName: 'rechnen'
+} );
