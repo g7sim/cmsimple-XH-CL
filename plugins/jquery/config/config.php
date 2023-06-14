5b3603f9f825ec98f47ea55184fa5d582d537e87
g7sim <regis@vollbio.de>
2023-06-14 14:06:42 +0200

    Update config.php

--- plugins/jquery/config/config.php
+++ plugins/jquery/config/config.php
@@ -2,7 +2,7 @@
 
 $plugin_cf['jquery']['version_core']="3.6.0";
 $plugin_cf['jquery']['version_ui']="1.13.2";
-$plugin_cf['jquery']['version_migrate']="jquery-migrate-4.0.min.js";
+$plugin_cf['jquery']['version_migrate']="jquery-migrate-3.4.1.min.js";
 $plugin_cf['jquery']['load_migrate']="true";
 $plugin_cf['jquery']['autoload']="true";
 $plugin_cf['jquery']['autoload_libraries']="jQuery";
